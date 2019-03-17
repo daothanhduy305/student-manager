9c01ed0d2a83b8b2aa93239148911f0376d7a143
ebolo <daothanhduy305@gmail.com>
2019-03-17 21:50:21 +0700

    Add Exit button to login view
    Signed-off-by: ebolo <daothanhduy305@gmail.com>

--- src/main/kotlin/com/ebolo/studentmanager/views/SMLoginFormView.kt
+++ src/main/kotlin/com/ebolo/studentmanager/views/SMLoginFormView.kt
@@ -2,31 +2,45 @@ package com.ebolo.studentmanager.views
 
 import com.ebolo.studentmanager.models.SMUserModel
 import com.ebolo.studentmanager.services.SMServiceCentral
+import javafx.application.Platform
 import javafx.geometry.Orientation
 import tornadofx.*
 
 class SMLoginFormView : View("StuMan v0.0.1-SNAPSHOT") {
     private val serviceCentral: SMServiceCentral by di()
 
-    val user = SMUserModel()
+    private val user = SMUserModel()
 
     override val root = form {
         fieldset("Trung tâm giảng dạy", labelPosition = Orientation.VERTICAL) {
             field("Tên người dùng") {
                 textfield(user.username).required()
             }
+
             field("Mật khẩu") {
                 passwordfield(user.password).required()
             }
-            button("Đăng nhập") {
-                action {
-                    if (serviceCentral.userService.login(user.getEntity())) {
-                        replaceWith<SMMainView>(
-                            sizeToScene = true,
-                            centerOnScreen = true
-                        )
 
-                        currentStage?.isMaximized = true
+            hbox(spacing = 10) {
+                paddingTop = 10
+
+                button("Đăng nhập") {
+                    action {
+                        if (serviceCentral.userService.login(user.getEntity())) {
+                            replaceWith<SMMainView>(
+                                sizeToScene = true,
+                                centerOnScreen = true
+                            )
+
+                            currentStage?.isMaximized = true
+                        }
+                    }
+                }
+
+                button("Thoát") {
+                    action {
+                        Platform.exit()
+                        System.exit(0)
                     }
                 }
             }
