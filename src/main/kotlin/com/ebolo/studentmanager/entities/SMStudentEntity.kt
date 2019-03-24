52300a24efb587d9074ba35cb742d1a171843452
ebolo <daothanhduy305@gmail.com>
2019-03-25 03:51:21 +0700

    Add title for the education level enum
    Signed-off-by: ebolo <daothanhduy305@gmail.com>

--- src/main/kotlin/com/ebolo/studentmanager/entities/SMStudentEntity.kt
+++ src/main/kotlin/com/ebolo/studentmanager/entities/SMStudentEntity.kt
@@ -9,13 +9,13 @@ class SMStudentEntity(
     var educationLevel: EducationLevel = EducationLevel.NONE
 ) : SMUserEntity()
 
-enum class EducationLevel {
-    PRE_SCHOOL,
-    PRIMARY,
-    SECONDARY,
-    HIGH_SCHOOL,
-    UNDERGRADUATE,
-    GRADUATED,
-    WORKING,
-    NONE
+enum class EducationLevel(val title: String) {
+    NONE(""),
+    PRE_SCHOOL("Mẫu giáo"),
+    PRIMARY("Tiểu học"),
+    SECONDARY("Trung học cơ sở"),
+    HIGH_SCHOOL("Trung học phổ thông"),
+    UNDERGRADUATE("Sinh viên"),
+    GRADUATED("Cử nhân"),
+    WORKING("Đang đi làm")
 }
