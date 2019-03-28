3fbe0c49c5e3ee0ebc7fbf691bc9485c8598248a
ebolo <daothanhduy305@gmail.com>
2019-03-28 21:29:32 +0700

    Add more field UI to student info view
    Signed-off-by: ebolo <daothanhduy305@gmail.com>

--- src/main/kotlin/com/ebolo/studentmanager/views/students/SMStudentInfoView.kt
+++ src/main/kotlin/com/ebolo/studentmanager/views/students/SMStudentInfoView.kt
@@ -3,8 +3,10 @@ package com.ebolo.studentmanager.views.students
 import com.ebolo.studentmanager.entities.EducationLevel
 import com.ebolo.studentmanager.models.SMStudentModel
 import com.ebolo.studentmanager.services.SMServiceCentral
+import javafx.beans.binding.Bindings
 import javafx.geometry.Orientation
 import javafx.scene.control.TabPane
+import javafx.scene.layout.Priority
 import tornadofx.*
 
 class SMStudentInfoView : View("Thông tin học viên") {
@@ -17,43 +19,68 @@ class SMStudentInfoView : View("Thông tin học viên") {
 
         tab("Thông tin cá nhân") {
             form {
-                fieldset(labelPosition = Orientation.HORIZONTAL) {
-                    field("Tên") {
-                        textfield(studentModel.firstName).required()
-                    }
+                hbox {
+                    vbox {
+                        fieldset(labelPosition = Orientation.HORIZONTAL) {
+                            field("Tên") {
+                                textfield(studentModel.firstName).required()
+                            }
 
-                    field("Họ và tên lót") {
-                        textfield(studentModel.lastName).required()
-                    }
+                            field("Họ và tên lót") {
+                                textfield(studentModel.lastName).required()
+                            }
 
-                    field("Nickname") {
-                        textfield(studentModel.nickname)
-                    }
+                            field("Nickname") {
+                                textfield(studentModel.nickname)
+                            }
 
-                    field("Ngày sinh") {
-                        datepicker(studentModel.birthday)
-                    }
+                            field("Ngày sinh") {
+                                datepicker(studentModel.birthday)
+                            }
 
-                    field("Số điện thoại") {
-                        textfield(studentModel.phone).required()
-                    }
+                            field("Số điện thoại") {
+                                textfield(studentModel.phone).required()
+                            }
 
-                    field("Số điện thoại phụ huynh") {
-                        textfield(studentModel.parentPhone)
-                    }
+                            field("Số điện thoại phụ huynh") {
+                                textfield(studentModel.parentPhone)
+                            }
 
-                    field("Địa chỉ") {
-                        textfield(studentModel.address)
-                    }
+                            field("Địa chỉ") {
+                                textfield(studentModel.address)
+                            }
 
-                    field("Học vấn") {
-                        combobox(
-                            studentModel.educationLevel,
-                            values = EducationLevel.values().toList()
-                        ) {
-                            cellFormat { text = it.title }
+                            field("Học vấn") {
+                                combobox(
+                                    studentModel.educationLevel,
+                                    values = EducationLevel.values().toList()
+                                ) {
+                                    cellFormat { text = it.title }
+                                }
+                            }
                         }
                     }
+
+                    vbox {
+                        paddingLeft = 15.0
+                        spacing = 10.0
+
+                        button("Hoàn tất") {
+                            vgrow = Priority.ALWAYS
+                            useMaxWidth = true
+
+                            enableWhen(Bindings.and(studentModel.dirty, studentModel.valid))
+                        }
+
+                        button("Hủy bỏ") {
+                            vgrow = Priority.ALWAYS
+                            useMaxWidth = true
+
+                            action { modalStage?.close() }
+                        }
+                    }
+
+                    width
                 }
             }
         }
