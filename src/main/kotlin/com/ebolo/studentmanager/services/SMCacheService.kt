0e090005ef23af261eae96f64c003bb0948cf5bc
saika <thaison1804@gmail.com>
2019-04-06 23:11:13 +0700

    Enable transaction to protect cache on sudden deaths
    Signed-off-by: saika <thaison1804@gmail.com>

--- src/main/kotlin/com/ebolo/studentmanager/services/SMCacheService.kt
+++ src/main/kotlin/com/ebolo/studentmanager/services/SMCacheService.kt
@@ -8,23 +8,38 @@ import org.springframework.stereotype.Service
 import javax.annotation.PostConstruct
 import javax.annotation.PreDestroy
 
-
+/**
+ * Service to handle operations regarding to caching / local db
+ *
+ * @author ebolo
+ * @since 0.0.1-SNAPSHOT
+ *
+ * @property db DB
+ * @property cache HTreeMap<String, Any>
+ */
 @Service
 class SMCacheService {
     private lateinit var db: DB
     lateinit var cache: HTreeMap<String, Any>
 
+    /**
+     * Method to setup components needed for this module
+     */
     @PostConstruct
     private fun setup() {
         db = DBMaker
             .fileDB(SMGlobal.CACHE_FILE)
             .fileMmapEnable()
+            .transactionEnable() // to protect the db on sudden deaths
             .make()
         cache = db
             .hashMap(SMGlobal.CACHE_NAME, Serializer.STRING, Serializer.JAVA)
             .createOrOpen()
     }
 
+    /**
+     * Method to handle operations to do on destroying this module
+     */
     @PreDestroy
     private fun onDestroyed() {
         db.close()
