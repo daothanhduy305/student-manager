cf6e5bb3026bfc9d78a982762d6296d638ef74c2
ebolo <daothanhduy305@gmail.com>
2019-04-14 21:03:04 +0700

    Make login form be more materialized
    Signed-off-by: ebolo <daothanhduy305@gmail.com>

--- src/main/kotlin/com/ebolo/studentmanager/views/SMLoginFormView.kt
+++ src/main/kotlin/com/ebolo/studentmanager/views/SMLoginFormView.kt
@@ -3,8 +3,10 @@ package com.ebolo.studentmanager.views
 import com.ebolo.studentmanager.models.SMUserModel
 import com.ebolo.studentmanager.services.SMServiceCentral
 import com.jfoenix.controls.JFXButton
+import com.jfoenix.controls.JFXTextField
 import javafx.application.Platform
 import javafx.geometry.Orientation
+import javafx.geometry.Pos
 import tornadofx.*
 
 class SMLoginFormView : View("StuMan v0.0.1-SNAPSHOT") {
@@ -13,17 +15,26 @@ class SMLoginFormView : View("StuMan v0.0.1-SNAPSHOT") {
     private val user = SMUserModel()
 
     override val root = form {
+        prefWidth = 300.0
+
         fieldset("Trung tâm giảng dạy", labelPosition = Orientation.VERTICAL) {
             field("Tên người dùng") {
-                textfield(user.username).required()
+                this += JFXTextField().apply {
+                    bind(user.username)
+                    required()
+                }
             }
 
             field("Mật khẩu") {
-                passwordfield(user.password).required()
+                this += JFXTextField().apply {
+                    bind(user.password)
+                    required()
+                }
             }
 
             hbox(spacing = 10) {
-                paddingTop = 10
+                paddingTop = 20
+                alignment = Pos.CENTER_RIGHT
 
                 this += JFXButton("Đăng nhập").apply {
                     buttonType = JFXButton.ButtonType.RAISED
