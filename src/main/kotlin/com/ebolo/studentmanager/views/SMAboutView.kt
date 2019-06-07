3c9ac43acfcb129604389cf3f3e555309fe7818b
Duy Đào Thanh <daothanhduy305@gmail.com>
2019-06-07 21:32:15 +0700

    Decorate About view
    Signed-off-by: Duy Đào Thanh <daothanhduy305@gmail.com>

--- src/main/kotlin/com/ebolo/studentmanager/views/SMAboutView.kt
+++ src/main/kotlin/com/ebolo/studentmanager/views/SMAboutView.kt
@@ -1,9 +1,51 @@
 package com.ebolo.studentmanager.views
 
+import javafx.geometry.Pos
 import tornadofx.*
 
 class SMAboutView : View("Giới thiệu") {
-    override val root = vbox {
+    override val root = borderpane {
+        setPrefSize(500.0, 300.0)
 
+        center {
+            hbox(spacing = 36) {
+                paddingAll = 20
+
+                vbox(spacing = 20) {
+                    alignment = Pos.CENTER
+
+                    stackpane {
+                        paddingRight = 16
+
+                        imageview("images/icon.png", true)
+                    }
+                }
+
+                vbox(spacing = 20) {
+                    alignment = Pos.CENTER_LEFT
+
+                    label("Student Manager") {
+                        style {
+                            fontSize = Dimension(24.0, Dimension.LinearUnits.pt)
+                        }
+                    }
+
+                    vbox(spacing = 10) {
+                        label("Phiên bản: v0.0.1") {
+                            style {
+                                fontSize = Dimension(12.0, Dimension.LinearUnits.pt)
+                            }
+                        }
+
+                        label("Tác giả: `ebolo` team") {
+                            style {
+                                fontSize = Dimension(12.0, Dimension.LinearUnits.pt)
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
     }
 }
