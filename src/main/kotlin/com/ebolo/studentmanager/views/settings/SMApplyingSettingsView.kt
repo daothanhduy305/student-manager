6238fbdfa3715432831bba049ba345a76a99c673
Duy Đào Thanh <daothanhduy305@gmail.com>
2019-06-08 09:21:56 +0700

    Decorate the settings application view a bit
    Signed-off-by: Duy Đào Thanh <daothanhduy305@gmail.com>

--- src/main/kotlin/com/ebolo/studentmanager/views/settings/SMApplyingSettingsView.kt
+++ src/main/kotlin/com/ebolo/studentmanager/views/settings/SMApplyingSettingsView.kt
@@ -1,5 +1,6 @@
 package com.ebolo.studentmanager.views.settings
 
+import javafx.geometry.Pos
 import tornadofx.*
 
 /**
@@ -15,7 +16,21 @@ class SMApplyingSettingsView : View("My View") {
         setPrefSize(500.0, 300.0)
 
         center {
-            label("Applying settings...")
+            vbox(spacing = 20) {
+                alignment = Pos.CENTER
+
+                stackpane {
+                    paddingRight = 16
+
+                    imageview("images/icon.png", true)
+                }
+
+                label("Đang xử lý...") {
+                    style {
+                        fontSize = Dimension(18.0, Dimension.LinearUnits.pt)
+                    }
+                }
+            }
         }
     }
 }
